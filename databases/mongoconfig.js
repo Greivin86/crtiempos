0aa68edbb76e027c1da84a4c8ccf7bd21065f752
Greivin A <g86alt@gmail.com>
2022-08-14 18:42:08 -0600

    databaseconfig

--- databases/mongoconfig.js
+++ databases/mongoconfig.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://mondo-db-tiempos-user:Grvn1986@tiemposapp0.xcgdhtp.mongodb.net/apuestas?retryWrites=true&w=majority";
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://mondo-db-tiempos-user:Grvn1986@tiemposapp0.xcgdhtp.mongodb.net/?retryWrites=true&w=majority";
