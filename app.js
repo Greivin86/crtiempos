5aa75e7bde225d5c9bcdfdd211e1027ff0ce3430
Greivin86 <g86alt@gmail.com>
2022-08-14 20:08:44 -0600

    session

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import methodOverride from "method-override";
-//import { MONGODB_URI, PORT } from "./databases/mongoconfig.js";
+import { MONGODB_URI, PORT } from "./databases/mongoconfig.js";
 import bodyParser from "body-parser";
+import MongoStore from "connect-mongo";
 import session from 'express-session';
 import router from './routes/index.js'
 
