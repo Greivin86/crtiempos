de371a85fff6596e01efea1ea9d5982e7c74750d
Greivin86 <g86alt@gmail.com>
2022-08-14 20:02:20 -0600

    session

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import methodOverride from "method-override";
 //import { MONGODB_URI, PORT } from "./databases/mongoconfig.js";
 import bodyParser from "body-parser";
-//import session from 'express-session';
+import session from 'express-session';
 import router from './routes/index.js'
 
 //init
@@ -15,14 +15,14 @@ app.set("port", process.env.PORT || 3000);
 
 //middlewares
 app.use(methodOverride('_method'))
-/*app.use(
+app.use(
     session({
         secret: "secret",
         resave: true,
         saveUninitialized: true,
         store: MongoStore.create({ mongoUrl: MONGODB_URI }),
     })
-);*/
+);
 app.use(bodyParser.urlencoded({
     extended: true
 }));
