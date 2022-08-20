2f1d678f855a4637d9863a8871374ef5a3654ea0
Greivin86 <g86alt@gmail.com>
2022-08-20 09:03:10 -0600

    insert pg working env port pg

--- databases/dbconfig.js
+++ databases/dbconfig.js
@@ -3,4 +3,4 @@ config();
 
 
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://mondo-db-tiempos-user:Grvn1986@tiemposapp0.xcgdhtp.mongodb.net/apuestas?retryWrites=true&w=majority";
-export const POSTGRES_URI = /*process.env.DATABASE_URL ||*/ "postgres://postgres:1234@localhost:5432/tiempos";
+export const POSTGRES_URI = process.env.DATABASE_URL || "postgres://postgres:1234@localhost:5432/tiempos";
